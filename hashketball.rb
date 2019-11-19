6ae9bda47e7777f476274fcce810924611235364
Rocky Pitto <rockypitto@gmail.com>
2019-11-19 20:04:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -209,7 +209,7 @@ def big_shoe_rebounds
   big_shoes_guy = 0
   rebounds = 0
     game_hash.each do | team, team_details_hash|
-      team_details_hash[:players].each do | stats |
+      team_details_hash[:players].each do |stats|
         if stats[:shoe] > big_shoes_guy
           big_shoes_guy = stats[:shoe]
           rebounds = stats[:rebounds]
